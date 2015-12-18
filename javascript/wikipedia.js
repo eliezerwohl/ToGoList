a3c06df21dc87a85f407f7f15f89ac757433f5ef
eliezerwohl <eliezerwohl@gmail.com>
2015-12-17 20:35:02 -0500

    now whats entered in, first two lines of wikipeda also come up

--- javascript/wikipedia.js
+++ javascript/wikipedia.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
   $(".btn").on("click", function(e){
     e.preventDefault()
+    var userInput = $("#searchInput").val();
     // var userInput = $("#searchInput").val();
-    var wikipediaUrl = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=37.786971|-122.399677&format=json";
-    // wikipediaUrl += "action=query&titles=Main%20Page&prop=revisions&rvprop=content";
-    // wikipediaUrl += "&format=json";
+    var wikipediaUrl = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&";
+    wikipediaUrl += "&search="+ userInput + "&format=json";
+
+
 
     $.ajax({
       type:"GET",
       url:wikipediaUrl,
        success: function (response){
-       console.log(response)
+       console.log(response[2][0])
+
        }
     });
 
   });
 });
+
+
+// do wikipedia searc
