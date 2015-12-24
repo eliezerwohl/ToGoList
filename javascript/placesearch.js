954517a0a6fee4a452a0ca69bfe31af1e837de02
C-Das <chinmay98@gmail.com>
2015-12-24 09:48:51 -0500

    updated placeservices to show search results

--- javascript/placesearch.js
+++ javascript/placesearch.js
@@ -16,24 +16,24 @@ $(document).ready(function() {
 
   function googleApiSuccessHandler(response) {
 
-    console.log(response);
-    console.log(response.results[0].place_id);
+    //console.log(response);
+    //console.log(response.results[0].place_id);
     
     createPlaceList(response);
 
-    var request = {
-      placeId: response.results[0].place_id
-    };
-    places = new google.maps.places.PlacesService($('#picturesDiv').get(0));
+    // var request = {
+    //   placeId: response.results[0].place_id
+    // };
+    // places = new google.maps.places.PlacesService($('#picturesDiv').get(0));
 
-    places.getDetails(request, callback);
+    // places.getDetails(request, callback);
 
-    function callback(place, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log("successful place services");
-        createPlacePhotos(place);
-      }
-    }
+    // function callback(place, status) {
+    //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //     console.log("successful place services");
+    //     createPlacePhotos(place);
+    //   }
+    // }
     //var geoLocation = response.results[0].geometry.location;
     // var flickrApiUrl = "https://api.flickr.com/services/rest/?";
     // var flickrApiParams = {
@@ -53,22 +53,27 @@ $(document).ready(function() {
   }
 
   function createPlaceList(response){
-    var pyrmont = new google.maps.LatLng(18.5204303,73.8567437);
+    var lat,lan;
+
+    lat= response.results[0].geometry.location.lat;
+    lan= response.results[0].geometry.location.lng;
+
+    var pyrmont = new google.maps.LatLng(lat,lan);
     var request = {
         location: pyrmont,
         radius: '100',
-        query: 'places to eat'
+        //types: 'campground|zoo|university|stadium|shopping_mall|restaurant|place_of_worship|park|museum|movie_theater'
       };
 
-      service = new google.maps.places.PlacesService($('#picturesDiv').get(0));
+      service = new google.maps.places.PlacesService($('#searchResultsDiv').get(0));
       service.nearbySearch(request, callback);
 
     function callback(results, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
+        //for (var i = 0; i < results.length; i++) { //To limit search to 20 results
+        for (var i = 0; i < 20; i++) {
           var place = results[i];
-          console.log(place);
-          //createMarker(results[i]);
+          createPlaceSearchResults(results[i]);
         }
       }
     }
@@ -76,13 +81,34 @@ $(document).ready(function() {
 
   function createPlacePhotos(place) {
 
-    console.log(place.PlacePhoto);
+    //console.log(place.PlacePhoto);
 
     // var locationPhotos = place.photos;
     // for(var i = 0; i < locationPhotos.length; i++) {
     //   var newCol = buildThumbnail(locationPhotos[i]);
     //   $("#photosRow").append(newCol);
     // }
+  }
+  function createPlaceSearchResults(place){
+    var newRow, searchTd, saveButton, saveTd,name,placeId;
+
+    console.log(place);
+
+    name = place.name;
+    placeId = place.place_id;
+
+    //console.log(name);
+
+    newRow = $("<tr>");
+    searchTd = $("<td>").addClass("searchResults").apped(name);
+    console.log(searchTd);
+    saveButton = $("<button>").addClass("btn btn-info").append("Save");
+    saveTd = $("<td>").append(saveButton);
+
+    newRow.append(searchTd).append(saveTd);
+    console.log (newRow);
+    $("tbody").append(newRow);
+    console.log("After the tbody call");
   }
 
 });
