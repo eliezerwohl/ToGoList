4b74f1d1ec413fb52434e46fe1a61b39b12d024b
eliezerwohl <eliezerwohl@gmail.com>
2016-01-02 14:41:26 -0500

    eliminated extra spaces in code

--- javascript/userlogin.js
+++ javascript/userlogin.js
@@ -5,10 +5,8 @@ window.userInfo = {
     userExists: "False" 
 };
 $(document).ready(function(){
-
   var loginOption, loginInput, userExists=0;
   var fbRefUsers;
-  
   $("#newUserRegister").hide();
   $("#SuccessfulLogin").hide();
   $(".mainContainer").hide();
@@ -16,7 +14,6 @@ $(document).ready(function(){
   // $(".animation").hide();
   $("#showSavedList").hide();
   $("#saveResultsDiv").hide();
-
   // //just for faster search page testing 
   // $("#userLoginContainer").hide();
   // $(".mainContainer").show();
@@ -25,9 +22,7 @@ $(document).ready(function(){
   // $(".jumbotron").hide();
   // $("#searchHeader").hide();
   // //End of Integration Code
-  
   var myFBRef = new Firebase("https://intense-inferno-5737.firebaseIO.com/");
-  
   $("#loginOption").on("focusout",function(){  
     loginOption = $("#loginOption").val();
   });
@@ -45,45 +40,36 @@ $(document).ready(function(){
     e.preventDefault();
     loginInput = $("#loginInput").val();
     validateEmail(loginInput);
-
     if (validateEmail(loginInput)===false){
       $("#validationMessage").html("Invalid E-mail. Please enter the e-mail correctly").css("color","red");
     } else {
       $("#validationMessage").html("");
       checkIfUserExists(loginInput);
-
     }
   });
 
   function registerUser(loginInput) {
     //var fName,lName,eMail;
-
     $("#userLogin").hide();
     $("#newUserRegister").show();
     $("#eMail").val(loginInput);
-
     $("#register").on("click",function(e){
       e.preventDefault();
-
-    userInfo.fName = $("#firstName").val();
-    userInfo.lName = $("#lastName").val();
-    userInfo.eMail = $("#eMail").val();
-
+      userInfo.fName = $("#firstName").val();
+      userInfo.lName = $("#lastName").val();
+      userInfo.eMail = $("#eMail").val();
       fbRefUsers.child(userInfo.fName).set({
         eMail : userInfo.eMail,
         fName : userInfo.fName,
         lName : userInfo.lName
       });
-
       $("#userInputForm").hide();
       $("#SuccessfulLogin").show();
       $("#validationMessage").html("Congratulations "+userInfo.fName+", your account has been successfully created.").css("color","black")
        .append("Click 'Start Search' to start your search").css("color","black");   
-   });
+    });
   }
-
   $("#startSearch").on("click",function(){
-
     $("#userloginRow").hide();
     $(".mainContainer").show();
     //$("#showSavedList").show();
@@ -92,7 +78,6 @@ $(document).ready(function(){
   });
   function checkIfUserExists (eMail) {
     var userList;
-
     fbRefUsers = myFBRef.child("users");
     fbRefUsers.orderByChild("eMail").equalTo(eMail).once("child_added", function(snapshot) { //child_added vs value
     //fbRefUsers.orderByChild("eMail").on("child_added", function(snapshot) {
@@ -107,7 +92,6 @@ $(document).ready(function(){
         $("#validationMessage").html("Welcome Back, "+userInfo.fName+". Click 'Start Search' to start your search").css("color","black");
       } 
     });
-
     fbRefUsers.orderByChild("eMail").equalTo(eMail).once("value", function(snapshot) { //child_added vs value
       if (snapshot.val() === null) {
         registerUser(loginInput); 
